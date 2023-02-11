a396a5b2d0b2cbcc59dc01d75364755d1bd71ec7
Koy Dibb <dibbk5@gmail.com>
2023-02-11 11:50:16 -0700

    Unit Tests

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,12 @@
 const {shuffleArray} = require('./utils')
 
+let testArray = ['Bob', 'Ted', 'Joe']
+
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test("shuffleArray is an array", () => {
+        expect(Array.isArray(shuffleArray(testArray))).toBe(true)
+    })
+    test("the same length of array is returned as passed in", () => {
+        expect(shuffleArray(testArray).length).toBe(testArray.length)
+    })
 })
